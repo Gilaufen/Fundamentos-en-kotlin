47ac24031fa6db9a2f469bbd4a981e48be09bd1b
Gilaufen <131225939+Gilaufen@users.noreply.github.com>
2023-08-06 17:27:17 -0500

    reto 1
    reto 1 completado

--- FundamentosKotlin/app/src/main/java/com/gilaufen/fundamentoskotlin/Reto1/Reto1.kt
+++ FundamentosKotlin/app/src/main/java/com/gilaufen/fundamentoskotlin/Reto1/Reto1.kt
@@ -4,5 +4,12 @@ class Reto1 {
 }
 
 fun main(){
-    println("Hola mundo")
+    println("Ingrese la cantidad de mensajes que ha recibido")
+    var nMensajes: Int= readLine()!!.toInt()
+
+    when(nMensajes){
+        0 -> println("No hay mensajes disponibles")
+        in 1..99 -> println("Ha recibido $nMensajes notificaciones")
+        in 100.. 1000 -> print("Ha recibido +99 mensajes")
+    }
 }
