32608c71d4a51342a636e31706730027662d2ddd
Gilaufen <[loreingp@hotmail.com]>
2023-10-04 11:08:50 -0500

    prueba update

--- NewProyect/app/src/main/java/com/gilaufen/newproyect/hola.kt
+++ NewProyect/app/src/main/java/com/gilaufen/newproyect/hola.kt
@@ -12,10 +12,26 @@ fun superficie(lado:Int):Int{
 fun datos(){
    println("Ingrese el valor del dado")
    val ladoRecogido = readln().toInt()
-   val calculoSuperficie = superficie(ladoRecogido)
+   val calculoSuperficie = supUnExpre(ladoRecogido)
    println("la superficie es ${calculoSuperficie}")
 }
 
 fun main(){
-   datos()
+   //datos()
+   //println("el numero mayor es :${mayor(1,2)}")
+   println("el numero es ${whenN(2)}")
+}
+
+//funciones de única expresión,son las que solo hacen una cosa y la podemos llamar en cualquier parte del código
+fun supUnExpre(lado: Int)=lado*lado
+
+//tambien en las funciones de unica expresión puedo hacer condicionales
+fun ifN(numero1:Int , numero2:Int)=if(numero1>numero2)numero1 else numero2
+
+fun whenN(numero1:Int) = when (numero1){
+      1-> "Uno"
+      2 -> "Dos"
+      3 -> "Tres"
+      4 -> "Cuatro"
+      else -> "Digite oto numero"
 }
