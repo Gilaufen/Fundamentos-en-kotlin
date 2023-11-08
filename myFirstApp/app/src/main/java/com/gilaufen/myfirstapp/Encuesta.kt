172e8cea16750783200568f338a8ea3941873e1a
Gilaufen <juancar2805@gmail.com>
2023-11-08 12:06:53 -0500

    UPDATE: file=( Encuesta )

---
+++ myFirstApp/app/src/main/java/com/gilaufen/myfirstapp/Encuesta.kt
@@ -0,0 +1,11 @@
+package com.gilaufen.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Encuesta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_encuesta)
+    }
+}
